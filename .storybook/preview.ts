8da429836011f5508777c1d50cb3219ef4f58cca
hi-yeonju <74940267+hi-yeonju@users.noreply.github.com>
2025-05-16 16:00:26 +0900

    feat: 토큰 전역 css 등록 기능 추가

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,4 +1,10 @@
 import type { Preview } from '@storybook/react'
+import { registerVariables } from '../src/design-system/tokens';
+
+import '../src/app/globals.css'; 
+
+// 토큰 css 전역변수로 등록
+registerVariables();
 
 const preview: Preview = {
   parameters: {
--- src/design-system/tokens/index.ts
+++ src/design-system/tokens/index.ts
@@ -1,1 +1,19 @@
 export * from './color'
+
+
+import { colors } from './color';
+
+// 토큰 css 전역변수로 등록
+export const registerVariables = () => {
+  const root = document.documentElement;
+
+  const categories = {
+    color: colors,
+  };
+
+  Object.entries(categories).forEach(([category, tokenSet]) => {
+    Object.entries(tokenSet).forEach(([key, value]) => {
+      root.style.setProperty(`--${category}-${key}`, value);
+    });
+  });
+};
